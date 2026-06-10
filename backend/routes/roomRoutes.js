8af27bab5e83b8ea403bed7d3d14db0d2ee03f2c
Vishnu Dholu <dholuvishnu10@gmail.com>
2026-06-10 22:30:15 +0530

    fixed async operations in room Routes

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -5,13 +5,13 @@ import { verifyToken } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 // Create Room
-router.post("/create", verifyToken, (req, res) => {
+router.post("/create", verifyToken, async (req, res) => {
     const { roomName, password } = req.body
     const userId = req.user.id
     const username = req.user.username;
 
     try {
-        const roomId = createRoom(userId, roomName, password, username)
+        const roomId = await createRoom(userId, roomName, password, username)
         res.status(201).json({ roomId })
     } catch (error) {
         res.status(500).json({ error: error.message })
@@ -19,13 +19,13 @@ router.post("/create", verifyToken, (req, res) => {
 })
 
 // Join Room
-router.post("/join", verifyToken, (req, res) => {
+router.post("/join", verifyToken, async (req, res) => {
     const { roomId, password } = req.body
     const userId = req.user.id
     const username = req.user.username;
 
     try {
-        const members = joinRoom(roomId, userId, password, username)
+        const members = await joinRoom(roomId, userId, password, username)
         res.status(200).json({ message: 'Joined room successfully', members, roomId })
     } catch (error) {
         res.status(400).json({ error: error.message })
